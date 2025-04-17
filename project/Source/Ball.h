caf0e73d536bdef54233ba5ccf0ed57f12abe460
satohiro <k235011@kccollege.ac.jp>
2025-04-17 13:13:31 +0900

    ボールの見た目変更、座標をボールの中心に変更

--- project/Source/Ball.h
+++ project/Source/Ball.h
@@ -10,14 +10,22 @@ class Physics;
 class Ball : public Object3D
 {
 public:
+	enum State
+	{
+		S_OWNED = 0,
+		S_THROWN
+	};
+
 	Ball();
 	~Ball();
 	void Update() override;
 	void Draw() override;
 
-	void SetVelocity(const Vector3& velocity);
+	void Throw(const Vector3& velocity);
 private:
 	Physics* m_Physics;
+	State m_State;
 
 	bool isHitFloor() const;
+	void setVelocity(const Vector3& velocity);
 };

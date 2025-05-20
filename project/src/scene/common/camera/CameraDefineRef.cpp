d7c944a710e422b18b943d0234af5fbd9b2d6231
mynameisGaku <154868516+mynameisGaku@users.noreply.github.com>
2025-05-20 18:07:02 +0900

    utilのフィルタ整理中

---
+++ project/src/scene/common/camera/CameraDefineRef.cpp
@@ -0,0 +1,34 @@
+#include "CameraDefineRef.h"
+#include "settings_json.h"
+
+#include "Util/Utils.h"
+
+void CameraDefineRef::Load(bool ForceLoad)
+{
+	if (not ForceLoad && m_WasLoad)
+	{
+		return;
+	}
+
+	// 諸々の初期化
+	auto jsonLoader = Settings_json::Inst();
+
+	// JSON 読み込み
+	jsonLoader->LoadSettingJson(FILEPATH, FILEKEY);
+
+	// 各種値取得
+	m_Near			= jsonLoader->GetOrDefault<float>("Near", 0.0f, FILEKEY);
+	m_Far			= jsonLoader->GetOrDefault<float>("Far", 0.0f, FILEKEY);
+	m_Perspective	= Math::DegToRad(jsonLoader->GetOrDefault<float>("Perspective", 0.0f, FILEKEY));
+	m_RotX_Max		= Math::DegToRad(jsonLoader->GetOrDefault<float>("RotX_Max", 0.0f, FILEKEY));
+	m_RotX_Min		= Math::DegToRad(jsonLoader->GetOrDefault<float>("RotX_Min", 0.0f, FILEKEY));
+	m_RotSpeedLimit	= Math::DegToRad(jsonLoader->GetOrDefault<float>("RotSpeedLimit", 0.0f, FILEKEY));
+
+	m_OffsetDef = jsonLoader->GetOrDefault<Vector3>("OffsetDef", V3::ZERO, FILEKEY);
+	m_TargetDef = jsonLoader->GetOrDefault<Vector3>("TargetDef", V3::ZERO, FILEKEY);
+	m_OffsetChase = jsonLoader->GetOrDefault<Vector3>("OffsetChase", V3::ZERO, FILEKEY);
+	m_TargetChase = jsonLoader->GetOrDefault<Vector3>("TargetChase", V3::ZERO, FILEKEY);
+
+	// ロードしたよ
+	m_WasLoad = true;
+}

639f30c42fbc618806426352c5f86f51570e6531
mynameisGaku <154868516+mynameisGaku@users.noreply.github.com>
2025-06-20 11:36:40 +0900

    スポナーに関連するファイルのリファクタリングを行いました。

--- project/src/scene/play/chara/CharaSpawnPoint.h
+++ project/src/scene/play/chara/CharaSpawnPoint.h
@@ -3,6 +3,10 @@
 #include "src/util/math/Vector3.h"
 #include "src/scene/play/spawner/SpawnerDescs.h"
 
+/// <summary>
+/// キャラのスポナー
+/// </summary>
+/// <author>藤本樂</author>
 class CharaSpawnPoint : public Object3D
 {
 public:
@@ -12,9 +16,11 @@ public:
     void Init(const Transform& trs, const CHARA_SPAWN_POINT_DESC& desc);
     void Draw() override;
 
+    // 使用回数を増やす
     void Use() { m_UseCount++; }
+    // 使用回数を取得
     int GetUseCount() const { return m_UseCount; }
 private:
-    Vector3 m_InitialVelocity;
-    int m_UseCount;
+    Vector3     m_InitialVelocity;  // 沸いたときに対象に与えられる初速
+    int         m_UseCount;         // 使用回数
 };
--- project/src/scene/play/chara/CharaSpawnPointManager.cpp
+++ project/src/scene/play/chara/CharaSpawnPointManager.cpp
@@ -16,6 +16,7 @@ CharaSpawnPointManager::~CharaSpawnPointManager()
 
 void CharaSpawnPointManager::Update()
 {
+    // csp = chara spawn point
     for (auto& csp : m_SpawnPoints)
     {
         csp->Update();
@@ -37,6 +38,7 @@ void CharaSpawnPointManager::Update()
 
 void CharaSpawnPointManager::Draw()
 {
+    // csp = chara spawn point
     for (auto& csp : m_SpawnPoints)
     {
         csp->Draw();
@@ -45,6 +47,7 @@ void CharaSpawnPointManager::Draw()
 
 void CharaSpawnPointManager::AddSpawnPoint(int hModel, const Transform& trs, const CHARA_SPAWN_POINT_DESC& desc)
 {
+    // csp = chara spawn point
     CharaSpawnPoint* csp = Instantiate<CharaSpawnPoint>();
     csp->Init(trs, desc);
     csp->SetModel(hModel);
@@ -55,6 +58,7 @@ CharaSpawnPoint* CharaSpawnPointManager::Get_Near(const Vector3& pos)
 {
     CharaSpawnPoint* result = nullptr;
     float len = std::numeric_limits<float>::max();
+    // csp = chara spawn point
     for (auto& csp : m_SpawnPoints)
     {
         Vector3 distance = csp->transform->position - pos;
@@ -72,6 +76,7 @@ CharaSpawnPoint* CharaSpawnPointManager::Get_Far(const Vector3& pos)
 {
     CharaSpawnPoint* result = nullptr;
     float len = 0;
+    // csp = chara spawn point
     for (auto& csp : m_SpawnPoints)
     {
         Vector3 distance = csp->transform->position - pos;
@@ -89,6 +94,7 @@ CharaSpawnPoint* CharaSpawnPointManager::Get_Random()
 {
     int r = Random.GetIntRange(0, m_SpawnPoints.size() - 1);
     int c = 0;
+    // csp = chara spawn point
     for (auto& csp : m_SpawnPoints)
     {
         if (c == r)
@@ -104,6 +110,7 @@ CharaSpawnPoint* CharaSpawnPointManager::Get_LowUsageRate()
 {
     CharaSpawnPoint* result = nullptr;
     int low = std::numeric_limits<int>::max();
+    // csp = chara spawn point
     for (auto& csp : m_SpawnPoints)
     {
         if (csp->GetUseCount() < low)
@@ -119,6 +126,7 @@ CharaSpawnPoint* CharaSpawnPointManager::Get_HighUsageRate()
 {
     CharaSpawnPoint* result = nullptr;
     int high = 0;
+    // csp = chara spawn point
     for (auto& csp : m_SpawnPoints)
     {
         if (csp->GetUseCount() > high)

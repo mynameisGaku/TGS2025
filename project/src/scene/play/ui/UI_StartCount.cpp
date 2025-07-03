2b54a063b3d369a0c6ffc1cd2034891c50216b99
satohiro <k235011@kccollege.ac.jp>
2025-07-03 11:58:10 +0900

    クラス追加

---
+++ project/src/scene/play/ui/UI_StartCount.cpp
@@ -0,0 +1,25 @@
+#include "UI_StartCount.h"
+#include "src/reference/game/GameRef.h"
+#include "src/scene/play/match/MatchManager.h"
+#include "src/util/string/StringUtil.h"
+#include "src/util/ui/UI_Manager.h"
+
+UI_StartCount::UI_StartCount()
+{
+}
+
+UI_StartCount::UI_StartCount(const RectTransform& trs, int index)
+{
+}
+
+UI_StartCount::~UI_StartCount()
+{
+}
+
+void UI_StartCount::Update()
+{
+}
+
+void UI_StartCount::Draw()
+{
+}

0a0295e1227e4a8b6e86e146660ed5a3a970aa5c
satohiro <k235011@kccollege.ac.jp>
2025-07-11 13:44:09 +0900

    モデルを差し替え

--- project/src/scene/play/chara/CharaManager.cpp
+++ project/src/scene/play/chara/CharaManager.cpp
@@ -170,7 +170,7 @@ CharaBase* CharaManager::Create(const std::string& tag, const Transform& trs)
 	if (tag == "Red")
 	{
 		hModel = ResourceLoader::MV1LoadModel("data/model/Chara/NoFaceGuy.mv1");
-		newChara->SetModelScale(Vector3::Ones);
+		newChara->SetModelScale(Vector3::Ones * 50.0f);
 
 		colParamChara.tag = ColDefine::Tag::tChara;
 		colParamChara.targetTags = { ColDefine::Tag::tChara, ColDefine::Tag::tBall, ColDefine::Tag::tTackle, ColDefine::Tag::tBlue };

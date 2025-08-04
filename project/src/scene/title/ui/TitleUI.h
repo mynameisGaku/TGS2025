98f4e13da7f33eaf64f4fee5312894cb65445fdd
mynameisGaku <154868516+mynameisGaku@users.noreply.github.com>
2025-08-04 18:26:30 +0900

    TitleUI.h を更新
    enumの命名が良くなかったと思いスマホから修正

--- project/src/scene/title/ui/TitleUI.h
+++ project/src/scene/title/ui/TitleUI.h
@@ -5,63 +5,63 @@
 #include <functional>
 
 /// <summary>
-/// �C�x���g���g���K�[����^�C�~���O
+/// イベントがトリガーするタイミング
 /// </summary>
 enum TUI_EVENT_TRIGGER_TIMING
 {
-	TUI_EVENT_TRIGGER_TIMING_HOLD,		// �������ςŃg���K�[����
-	TUI_EVENT_TRIGGER_TIMING_ENTER,		// �������u�Ԃ����g���K�[����
-	TUI_EVENT_TRIGGER_TIMING_RELEASE,	// �������Ƃ��Ƀg���K�[����
+	TUI_EVENT_TRIGGER_TIMING_HOLD,		// 押しっぱでトリガーする
+	TUI_EVENT_TRIGGER_TIMING_ENTER,		// 押した瞬間だけトリガーする
+	TUI_EVENT_TRIGGER_TIMING_RELEASE,	// 離したときにトリガーする
 };
 
 /// <summary>
-/// �����蔻����@
+/// 当たり判定方法
 /// </summary>
-enum TUI_COLLISION
+enum TUI_COLLISION_MODE
 {
-	TUI_COLLISION_NONE,		// �����蔻��Ȃ�
-	TUI_COLLISION_CIRCLE,	// �~�̓����蔻��
-	TUI_COLLISION_RECT,		// �l�p�`�̓����蔻��
+	TUI_COLLISION_MODE_NONE,		// 当たり判定なし
+	TUI_COLLISION_MODE_CIRCLE,	// 円の当たり判定
+	TUI_COLLISION_MODE_RECT,		// 四角形の当たり判定
 };
 
 /// <summary>
-/// �C�x���g��̂܂Ƃ܂�
+/// イベント一つのまとまり
 /// </summary>
 typedef struct 
 {
-	TUI_EVENT_TRIGGER_TIMING	TIMING					{};	// �ǂ̃^�C�~���O�Ńg���K�[����̂�
-	const		TCHAR*			EVENT					{};	// �C�x���g�̖��O�B������Ŏw��BTitleUIController�ɓo�^����Ă���}�b�v�Əƍ������B
-	bool						IS_WAIT_FOR_COMPLETION	{};	// ���s�����������܂ő҂��ǂ����̃t���O
-	const		TCHAR*			DESCRIPTION				{};	// ���̃C�x���g�̋L�q�B�قڃf�o�b�O�p
+	TUI_EVENT_TRIGGER_TIMING	TIMING					{};	// どのタイミングでトリガーするのか
+	const		TCHAR*			EVENT					{};	// イベントの名前。文字列で指定。TitleUIControllerに登録されているマップと照合される。
+	bool						IS_WAIT_FOR_COMPLETION	{};	// 実行が完了されるまで待つかどうかのフラグ
+	const		TCHAR*			DESCRIPTION				{};	// このイベントの記述。ほぼデバッグ用
 }TUI_EVENT;
 
 /// <summary>
-/// �^�C�g��UI�̋L�q
+/// タイトルUIの記述
 /// </summary>
 typedef struct
 {
-	int						GRAPH_HANDLE	{};	// �摜�̃n���h��
-	int						GRAPH_POS_X		{};	// �`��J�n�ʒu x
-	int						GRAPH_POS_Y		{};	// �`��J�n�ʒu y
-	int						GRAPH_SRC_X		{};	// �摜�؂蔲���J�n�ʒu x
-	int						GRAPH_SRC_Y		{};	// �摜�؂蔲���J�n�ʒu y
-	int						GRAPH_DEST_X	{};	// �摜�؂蔲���I���ʒu x
-	int						GRAPH_DEST_Y	{};	// �摜�؂蔲���I���ʒu y
-	int						INDEX_X			{};	// Canvas��œ�����Cursor�p��Index x
-	int						INDEX_Y			{};	// Canvas��œ�����Cursor�p��Index y
-	std::list<TUI_EVENT>	EVENTS			{};	// ����UI���N���b�N�����Ƃ��ɋN����C���x���g���X�g�B�ǉ����ꂽ���ɏ�������܂��B
-	TUI_COLLISION			COLLISION		{};	// ����UI�̓����蔻����@
-	const		TCHAR*		DESCRIPTION		{};	// ����UI�Ɋւ���L�q�B�T�v�Ƃ��B�f�o�b�O�p
+	int						GRAPH_HANDLE	{};	// 画像のハンドラ
+	int						GRAPH_POS_X		{};	// 描画開始位置 x
+	int						GRAPH_POS_Y		{};	// 描画開始位置 y
+	int						GRAPH_SRC_X		{};	// 画像切り抜き開始位置 x
+	int						GRAPH_SRC_Y		{};	// 画像切り抜き開始位置 y
+	int						GRAPH_DEST_X	{};	// 画像切り抜き終了位置 x
+	int						GRAPH_DEST_Y	{};	// 画像切り抜き終了位置 y
+	int						INDEX_X			{};	// Canvas上で動かすCursor用のIndex x
+	int						INDEX_Y			{};	// Canvas上で動かすCursor用のIndex y
+	std::list<TUI_EVENT>	EVENTS			{};	// このUIをクリックしたときに起こるインベントリスト。追加された順に処理されます。
+	TUI_COLLISION_MODE			COLLISION		{};	// このUIの当たり判定方法
+	const		TCHAR*		DESCRIPTION		{};	// このUIに関する記述。概要とか。デバッグ用
 }UI_TITLE_DESC;
 
 
-// �O���錾
+// 前方宣言
 class TitleUICanvas;
 
 /// <summary>
-/// �^�C�g��UI
+/// タイトルUI
 /// </summary>
-/// <author> ���{ </author>
+/// <author> 藤本 </author>
 class TitleUI
 {
 public:
@@ -72,40 +72,40 @@ public:
 	void	Release();
 
 	///		<summary>
-	///		�L�q�����ɏ���������
+	///		記述を元に初期化する
 	///		</summary>
 	void	Init(const UI_TITLE_DESC& desc, TitleUICanvas* pCanvas);
 
 	///		<summary>
-	///		����UI���������ςȂ��ɂ��Ă�Ƃ��ɌĂ΂��
+	///		このUIを押しっぱなしにしてるときに呼ばれる
 	///		</summary>
 	void	OnPressHold();
 
 	///		<summary>
-	///		����UI���������u�ԂɌĂ΂��
+	///		このUIを押した瞬間に呼ばれる
 	///		</summary>
 	void	OnPressEnter();
 
 	///		<summary>
-	///		����UI�𗣂����Ƃ��ɌĂ΂��
+	///		このUIを離したときに呼ばれる
 	///		</summary>
 	void	OnPressRelease();
 private:
-	int								m_GraphHandle			{};	// �摜�̃p�X ���łɃ��[�h�ς݂̉摜���w�肷��ꍇ�͕s�v
-	int								m_GraphPosX				{};	// �`��J�n�ʒu x
-	int								m_GraphPosY				{};	// �`��J�n�ʒu y
-	int								m_GraphSrcX				{};	// �摜�؂蔲���J�n�ʒu x
-	int								m_GraphSrcY				{};	// �摜�؂蔲���J�n�ʒu y
-	int								m_GraphDestX			{};	// �摜�؂蔲���I���ʒu x
-	int								m_GraphDestY			{};	// �摜�؂蔲���I���ʒu y
-	bool							m_IsActive				{};	// �L�����H
-	bool							m_IsVisible				{};	// �\������Ă��邩�H
-	bool							m_IsHolding				{};	// ��������Ă��邩�H
-	bool							m_IsHover				{};	// �J�[�\��������UI�̏�ɂ��邩�H
-	const TCHAR*					m_Description			{};	// ����UI�Ɋւ���L�q
-	std::list<TUI_EVENT>			m_PressHoldEvents		{};	// ����UI�𒷉��������Ƃ��ɋN����C���x���g���X�g�B�ǉ����ꂽ���ɏ�������܂��B
-	std::list<TUI_EVENT>			m_PressEnterEvents		{};	// ����UI���������u�ԂɋN����C���x���g���X�g�B�ǉ����ꂽ���ɏ�������܂��B
-	std::list<TUI_EVENT>			m_PressReleaseEvents	{};	// ����UI����N���b�N�𗣂����Ƃ��ɋN����C���x���g���X�g�B�ǉ����ꂽ���ɏ�������܂��B
-	TUI_COLLISION					m_Collision				{};	// ����UI�̓����蔻����@�B�f�t�H���g�͂Ȃ��B�N���b�N�C�x���g�𔭉΂����邽�߂ɕK�v�B
-	TitleUICanvas*					m_pHolder				{};	// ����UI��ێ����Ă���L�����o�X
+	int								m_GraphHandle			{};	// 画像のパス すでにロード済みの画像を指定する場合は不要
+	int								m_GraphPosX				{};	// 描画開始位置 x
+	int								m_GraphPosY				{};	// 描画開始位置 y
+	int								m_GraphSrcX				{};	// 画像切り抜き開始位置 x
+	int								m_GraphSrcY				{};	// 画像切り抜き開始位置 y
+	int								m_GraphDestX			{};	// 画像切り抜き終了位置 x
+	int								m_GraphDestY			{};	// 画像切り抜き終了位置 y
+	bool							m_IsActive				{};	// 有効か？
+	bool							m_IsVisible				{};	// 表示されているか？
+	bool							m_IsHolding				{};	// 押下されているか？
+	bool							m_IsHover				{};	// カーソルがこのUIの上にあるか？
+	const TCHAR*					m_Description			{};	// このUIに関する記述
+	std::list<TUI_EVENT>			m_PressHoldEvents		{};	// このUIを長押ししたときに起こるインベントリスト。追加された順に処理されます。
+	std::list<TUI_EVENT>			m_PressEnterEvents		{};	// このUIを押した瞬間に起こるインベントリスト。追加された順に処理されます。
+	std::list<TUI_EVENT>			m_PressReleaseEvents	{};	// このUIからクリックを離したときに起こるインベントリスト。追加された順に処理されます。
+	TUI_COLLISION_MODE					m_Collision				{};	// このUIの当たり判定方法。デフォルトはなし。クリックイベントを発火させるために必要。
+	TitleUICanvas*					m_pHolder				{};	// このUIを保持しているキャンバス
 };
